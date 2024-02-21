a4ac4b9a3e076e9f6eee6270433a553285639ab5
DImiir <d.i.mironenko@yandex.ru>
2024-02-21 14:38:01 +0300

    задача "Автоматический ответ"

--- task/flaskll.py
+++ task/flaskll.py
@@ -22,6 +22,19 @@ def listing(list):
     return render_template('list.html', lst=prof_list, type=list)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    dct = {
+        'title': 'Анкета', 'surname': 'Watny', 'name': 'Mark', 'education': 'выше среднего',
+        'profession': 'штурман марсохода', 'sex': 'male', 'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': 'True'
+    }
+    return render_template('auto_answer.html', title=dct['title'], surname=dct['surname'],
+                           name=dct['name'], education=dct['education'], profession=dct['profession'], sex=dct['sex'],
+                           motivation=dct['motivation'], ready=dct['ready'])
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
 
