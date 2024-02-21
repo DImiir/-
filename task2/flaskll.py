27cf730dfbe7789b1f150bf4dc96512a142679e9
DImiir <d.i.mironenko@yandex.ru>
2024-02-21 10:11:52 +0300

    задача "Список профессий"

--- task2/flaskll.py
+++ task2/flaskll.py
@@ -13,6 +13,15 @@ def training(prof):
     return render_template('train.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def listing(list):
+    prof_list = [
+        'инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'климатолог', 'гляциолог', 'штурман',
+        'инженер по терраформированию', 'специалист по информационной защите', 'киберинженер', 'астрогеолог'
+    ]
+    return render_template('list.html', lst=prof_list, type=list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
 
